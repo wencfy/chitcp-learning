1df38387780c96ddfc2032bc045c200988f9f51a
wen <wen.wa@qq.com>
2022-12-01 20:10:14 +0800

    feat (*): copy from uchicago-cs/chitcp

---
+++ src/libchitcp/multitimer.c
@@ -0,0 +1,168 @@
+/*
+ *  chiTCP - A simple, testable TCP stack
+ *
+ *  An API for managing multiple timers
+ */
+
+/*
+ *  Copyright (c) 2013-2019, The University of Chicago
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of The University of Chicago nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "chitcp/multitimer.h"
+#include "chitcp/log.h"
+
+
+
+/* See multitimer.h */
+int timespec_subtract(struct timespec *result, struct timespec *x, struct timespec *y)
+{
+    struct timespec tmp;
+    tmp.tv_sec = y->tv_sec;
+    tmp.tv_nsec = y->tv_nsec;
+
+    /* Perform the carry for the later subtraction by updating tmp. */
+    if (x->tv_nsec < tmp.tv_nsec) {
+        uint64_t sec = (tmp.tv_nsec - x->tv_nsec) / SECOND + 1;
+        tmp.tv_nsec -= SECOND * sec;
+        tmp.tv_sec += sec;
+    }
+    if (x->tv_nsec - tmp.tv_nsec > SECOND) {
+        uint64_t sec = (x->tv_nsec - tmp.tv_nsec) / SECOND;
+        tmp.tv_nsec += SECOND * sec;
+        tmp.tv_sec -= sec;
+    }
+
+    /* Compute the time remaining to wait.
+       tv_nsec is certainly positive. */
+    result->tv_sec = x->tv_sec - tmp.tv_sec;
+    result->tv_nsec = x->tv_nsec - tmp.tv_nsec;
+
+    /* Return 1 if result is negative. */
+    return x->tv_sec < tmp.tv_sec;
+}
+
+
+/* See multitimer.h */
+int mt_init(multi_timer_t *mt, uint16_t num_timers)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
+
+
+/* See multitimer.h */
+int mt_free(multi_timer_t *mt)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
+
+
+/* See multitimer.h */
+int mt_get_timer_by_id(multi_timer_t *mt, uint16_t id, single_timer_t **timer)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
+
+
+/* See multitimer.h */
+int mt_set_timer(multi_timer_t *mt, uint16_t id, uint64_t timeout, mt_callback_func callback, void* callback_args)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
+
+
+/* See multitimer.h */
+int mt_cancel_timer(multi_timer_t *mt, uint16_t id)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
+
+
+/* See multitimer.h */
+int mt_set_timer_name(multi_timer_t *mt, uint16_t id, const char *name)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
+
+
+/* mt_chilog_single_timer - Prints a single timer using chilog
+ *
+ * level: chilog log level
+ *
+ * timer: Timer
+ *
+ * Returns: Always returns CHITCP_OK
+ */
+int mt_chilog_single_timer(loglevel_t level, single_timer_t *timer)
+{
+    struct timespec now, diff;
+    clock_gettime(CLOCK_REALTIME, &now);
+
+    if(timer->active)
+    {
+        /* Compute the appropriate value for "diff" here; it should contain
+         * the time remaining until the timer times out.
+         * Note: The timespec_subtract function can come in handy here*/
+        diff.tv_sec = 0;
+        diff.tv_nsec = 0;
+        chilog(level, "%i %s %lis %lins", timer->id, timer->name, diff.tv_sec, diff.tv_nsec);
+    }
+    else
+        chilog(level, "%i %s", timer->id, timer->name);
+
+    return CHITCP_OK;
+}
+
+
+/* See multitimer.h */
+int mt_chilog(loglevel_t level, multi_timer_t *mt, bool active_only)
+{
+    /* Your code here */
+
+    return CHITCP_OK;
+}
